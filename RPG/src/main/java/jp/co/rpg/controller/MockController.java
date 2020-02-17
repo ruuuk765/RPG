bde57e43646c46ae442e9959233fa7b81c9b5586
s14333591 <58578037+s14333591@users.noreply.github.com>
2020-02-17 16:23:49 +0900

    マージ

--- RPG/src/main/java/jp/co/rpg/controller/MockController.java
+++ RPG/src/main/java/jp/co/rpg/controller/MockController.java
@@ -1,35 +1,14 @@
 package jp.co.rpg.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MockController {
-<<<<<<< HEAD
-=======
-	@RequestMapping("/index")
-	public String index() {
-		return "index";
-	}
-//
-//	@RequestMapping("/regist")
-//	public String regist() {
-//		return "regist";
-//	}
-//	@RequestMapping("/test")
-//	public String test() {
-//		return "registConfirm";
-//	}
->>>>>>> branch 'master' of https://github.com/ruuuk765/RPG.git
 
-<<<<<<< HEAD
-=======
 //	@RequestMapping("/login")
 //	public String login() {
 //		return "login";
 //	}
->>>>>>> branch 'master' of https://github.com/ruuuk765/RPG.git
-
 //	@RequestMapping("/selectRole")
 //	public String selectRole() {
 //		return "selectRole";
