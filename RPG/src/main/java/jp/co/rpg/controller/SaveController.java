023aedfb83ecc162e114dff86fdb61289caab03f
hoge <hoge@xxx.jp>
2020-02-17 16:35:32 +0900

    create saveControler

--- RPG/src/main/java/jp/co/rpg/controller/SaveController.java
+++ RPG/src/main/java/jp/co/rpg/controller/SaveController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.rpg.entity.User;
+
 @Controller
 public class SaveController {
 	@Autowired
