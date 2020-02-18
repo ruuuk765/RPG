29e2d7ebfbcfcdab1ad3900b13fba4a448dd43d7
s14333591 <58578037+s14333591@users.noreply.github.com>
2020-02-18 11:30:08 +0900

    「やすむ」機能実装、メッセージ分岐未実装

---
+++ RPG/src/main/java/jp/co/rpg/controller/RestController.java
@@ -0,0 +1,32 @@
+package jp.co.rpg.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.rpg.entity.User;
+import jp.co.rpg.service.RestService;
+
+@Controller
+public class RestController {
+
+	@Autowired
+	HttpSession session;
+	@Autowired
+	RestService service;
+
+	User user = new User();
+
+
+	@RequestMapping("/rest")
+	public String rest() {
+
+		user = (User) session.getAttribute("user");
+
+		Boolean result = service.rest(user);
+
+		return "home";
+	}
+}
---
+++ RPG/src/main/java/jp/co/rpg/service/impl/RestServiceImpl.java
@@ -0,0 +1,26 @@
+package jp.co.rpg.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import jp.co.rpg.entity.User;
+import jp.co.rpg.service.RestService;
+
+@Service
+public class RestServiceImpl implements RestService {
+
+	private final Integer isRestPrice = 500;
+
+	@Override
+	public Boolean rest(User user) {
+
+		if(user.getGold() >= isRestPrice) {
+			user.setHp(user.getMaxHp());
+			user.setMp(user.getMaxMp());
+			user.setGold(((user.getGold())-isRestPrice));
+			return true;
+		}
+		return false;
+	}
+
+}
+
